7dad82fdc0e61578be00a18fe72d8bdd98d3568d
Will Gundy <willgunderson503@gmail.com>
2022-03-16 16:42:40 -0700

    added rendering for mushrooms

--- app.js
+++ app.js
@@ -83,9 +83,10 @@ function displayFriends() {
 
 function displayMushrooms() {
     // clear out the mushroom div
-
+    mushroomsEl.innerHTML = '';
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
+        mushroomsEl.append(renderMushroom());
     }
 }
 
